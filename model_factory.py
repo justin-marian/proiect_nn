497bd33d99f0c3d1c3376a16c7083f1f6dfafd63
AlfredPietraru <alfred.andrei@yahoo.com>
2025-12-07 19:14:08 +0200

    final_version

--- model_factory.py
+++ model_factory.py
@@ -1,13 +1,17 @@
 
 import torch
 import torch.nn as nn
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models import ResNet50_Weights 
 
 class FasterRCNN_RESNET50_FPN(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.model = fasterrcnn_resnet50_fpn(weights_backbone=ResNet50_Weights.IMAGENET1K_V2)
+        # self.model = fasterrcnn_resnet50_fpn(weights_backbone=ResNet50_Weights.IMAGENET1K_V2)
+        self.model = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+        in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 21)
 
     def forward(self, images, targets):
         return self.model(images, targets)
@@ -15,6 +19,4 @@ class FasterRCNN_RESNET50_FPN(nn.Module):
 def get_model(device):
     model = FasterRCNN_RESNET50_FPN()
     return model.to(device)
-
-
 
